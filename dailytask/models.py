b4524226bff59a38d2149abcc9dfa381708789ff
C.y <yangruihan@vip.qq.com>
2016-03-12 20:19:18 +0800

    Yrh Web Site. Daily Task Module v0.0.2
    1. 修复部分设置错误
    2. 修改 dailytask __init__.py 使用 pymysql
    3. 添加 Task 模型，并生成数据库表

--- dailytask/models.py
+++ dailytask/models.py
@@ -8,7 +8,7 @@ class Task(models.Model):
     parent = models.IntegerField(default=0) # 父任务id，默认为0
     create_date = models.DateTimeField(auto_now_add=True) # 任务创建时间
     done_date = models.DateTimeField(blank=True) # 任务完成时间
-    have_compeleted = BooleanField(default=False) # 是否已经完成
+    have_compeleted = models.BooleanField(default=False) # 是否已经完成
     
     def __str__(self):
         return self.content
