16d7ba60f411cff03ab3b59ace956a28ef4fda5e
C.y <yangruihan@vip.qq.com>
2016-03-12 20:09:24 +0800

    Yrh Web Site. Daily Task Module v0.0.1
    初始化

---
+++ dailytask/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Task(models.Model):
+    """
+    任务类
+    """
+    content = models.CharField(max_length=255) # 任务内容
+    parent = models.IntegerField(default=0) # 父任务id，默认为0
+    create_date = models.DateTimeField(auto_now_add=True) # 任务创建时间
+    done_date = models.DateTimeField(blank=True) # 任务完成时间
+    have_compeleted = BooleanField(default=False) # 是否已经完成
+    
+    def __str__(self):
+        return self.content
