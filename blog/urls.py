0b9c072b5c30d5defd58bcec3efc0f8b99fefcac
C.y. <yangruihan@vip.qq.com>
2016-03-22 19:12:16 +0800

    Yrh Web Site. v0.3.5
    1. 点击右侧分类可以显示该分类的文章信息

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index_view, name='index'),
+    url(r'^index/$', views.index_view, name='index'),
     url(r'^article/(?P<article_id>[0-9]+)/$', views.detail_view, name='article_detail'),
     url(r'^new_article/$', views.new_article_view, name='new_article_view'),
     url(r'^do_new_category/$', views.do_new_category, name='do_new_category'),
     url(r'^do_create_article/$', views.do_create_article, name='do_create_article'),
+    url(r'^articles/category/(?P<category_id>[0-9]+)/', views.do_show_articles_by_category, name='do_show_articles_by_category'),
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -159,4 +159,29 @@ def do_create_article(request):
                                                             'message': '新建文章成功'})
     except Exception as e:
         print(e)
+        
+
+@login_required(login_url='/login')
+def do_show_articles_by_category(request, category_id):
+    user = request.user
+    articles = Article.objects.filter(user=user,
+                                      category__id=category_id).order_by('date_time')
+    paginator = Paginator(articles, 10)  # 每页显示个数
     
+    # 统计分类中文章的个数
+    category_counter = get_category_counter(user)
+    
+    # 获得使用到的标签
+    tag_counter = get_tag_counter(user)
+    
+    page = request.GET.get('page')
+    
+    try:
+        article_list = paginator.page(page)
+    except PageNotAnInteger:
+        article_list = paginator.page(1)
+    except EmptyPage:
+        article_list = paginator.paginator(paginator.num_pages)
+    return render(request, 'blog/index.html', {'articles': articles,
+                                               'category_counter': category_counter,
+                                               'tag_counter': tag_counter})
